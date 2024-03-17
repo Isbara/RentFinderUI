89d7d60d7ada5b642e6999ee20142f0f1b3910c2
Turgut Canberk Diner <canbodiner@hotmail.com>
2024-03-17 20:30:44 +0300

    Minor Changes
    Minor Changes

--- src/HeaderNotLogged.js
+++ src/HeaderNotLogged.js
@@ -5,10 +5,7 @@ function HeaderNotLogged() {
     return (
         <header className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <Link to="/" className="navbar-brand">RENT FINDER</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <img src="/Logo.png" alt="Logo" width="230" height="50" />
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
