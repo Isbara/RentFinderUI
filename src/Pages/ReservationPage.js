bbe36e83f75be94b1b0a25089cb3587e13f227ee
Isbara <isbaralpyildirim@gmail.com>
2024-03-24 18:25:45 +0200

    Meeting push
    ReservationPage fixed

--- src/Pages/ReservationPage.js
+++ src/Pages/ReservationPage.js
@@ -5,7 +5,7 @@ function ReservationPage({ getToken }) {
     const token = getToken();
     const isLoggedIn = token;
     const [reservations, setReservations] = useState([]);
-    const [description, setComment] = useState('');
+    const [description, setDescription] = useState('');
 
     useEffect(() => {
         fetchUserReservations();
@@ -22,21 +22,21 @@ function ReservationPage({ getToken }) {
                 }
             });
             const data = await response.json();
-            setReservations(data);
+            await setReservations(data);
         } catch (error) {
             console.error('Error fetching user reservations:', error);
         }
     };
 
     const handleCommentChange = (e) => {
-        setComment(e.target.value);
+        setDescription(e.target.value);
     };
 
     const handleSubmitComment = async (propertyID, reservationID) => {
         console.log(propertyID)
         console.log(reservationID)
         try {
-            const response = await fetch(`http://localhost:8080/review/${propertyID}/${reservationID}`, {
+            const response = await fetch("http://localhost:8080/review/" + propertyID + "/" + reservationID, {
                 method: 'POST',
                 body: JSON.stringify({ description }),
                 headers: {
@@ -62,33 +62,35 @@ function ReservationPage({ getToken }) {
             <div className="container mt-4">
                 <h1 className="mb-4">User Reservations</h1>
                 <div className="row">
-                    {reservations.map(reservation => (
-                        <div key={reservation.reservationID} className="col-md-4 mb-4">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Reservation ID: {reservation.reservationID}</h5>
-                                    <p className="card-text">Number of People: {reservation.numberOfPeople}</p>
-                                    <p className="card-text">Start Date: {reservation.startDate}</p>
-                                    <p className="card-text">End Date: {reservation.endDate}</p>
-                                    <div className="form-group">
-                                        <textarea
-                                            className="form-control"
-                                            rows="3"
-                                            placeholder="Write your comment..."
-                                            value={description}
-                                            onChange={handleCommentChange}
-                                        ></textarea>
+                    {reservations.map(reservation => {
+                        return(
+                            <div key={reservation?.reservationID} className="col-md-4 mb-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Reservation ID: {reservation?.reservationID}</h5>
+                                        <p className="card-text">Number of People: {reservation?.numberOfPeople}</p>
+                                        <p className="card-text">Start Date: {reservation?.startDate}</p>
+                                        <p className="card-text">End Date: {reservation?.endDate}</p>
+                                        <div className="form-group">
+                                            <textarea
+                                                className="form-control"
+                                                rows="3"
+                                                placeholder="Write your comment..."
+                                                value={description}
+                                                onChange={handleCommentChange}
+                                            ></textarea>
+                                        </div>
+                                        <button
+                                            className="btn btn-primary"
+                                            onClick={() => handleSubmitComment(reservation?.propertyID,reservation?.reservationID)}
+                                        >
+                                            Add Comment
+                                        </button>
                                     </div>
-                                    <button
-                                        className="btn btn-primary"
-                                        onClick={() => handleSubmitComment(reservation.propertyID,reservation.reservationID)}
-                                    >
-                                        Add Comment
-                                    </button>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        )
+                    })}
                 </div>
             </div>
         </div>
